ea0118ff6974f545b99e22c4ed664b9b891394cf
TheoWavelet <39880052+TheoWavelet@users.noreply.github.com>
2023-01-16 09:52:05 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-kfwma9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-kfwma9)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject, OnInit, VERSION } from '@angular/core';
+import { AppService } from './app.service';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  appService = inject(AppService);
+
+  ngOnInit(): void {
+    this.appService.getGridData().subscribe({
+      next: (data) => console.log(data),
+      error: (error) => console.log('error', error.message),
+    });
+  }
+}
